b9f1b4237d10b7abcd81a93b7821c5db74489311
Decong Yu <decong.yu@gmail.com>
2022-05-29 15:11:04 -0600

    short circuit, multi-return, shoe/hide

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,53 @@
 import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+  const [loading, setLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] =useState('default user');
+
+  useEffect(() => {
+    fetch(url)
+      .then((resp)=> {
+        if(resp.status >= 200 && resp.status <= 299){
+          return resp.json();
+        } else  {
+          setLoading(false);
+          setIsError(true);
+          return (
+            resp.statusText // hanle HTTP 404 Error
+          )
+        }
+      })
+      .then((user) => {
+        const { login } = user;
+        setUser(login);
+        setLoading(false);
+      })
+      // .catch((error) => {
+      //   setIsError(error)
+      // });
+  }, []);
+  
+  if (loading) {
+    return (
+      <div>
+        <h1>Loading</h1>
+      </div>
+    );
+  }
+  if (isError) {
+    return (
+      <div>
+        <h1>Error...</h1>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h2>{user}</h2>
+    </div>
+  );
 };
 
 export default MultipleReturns;
--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,10 +3,30 @@ import React, { useState } from 'react';
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState(''); // empty text vs. a text
+  const [isError, setIsError] = useState(false);
 
-  return <h2>short circuit</h2>;
+  // const firstValue = text || 'hello world'; // or operator
+  // const secondValue = text && 'hello world'; // if (text) operator
+
+  return (
+    <>
+      {/* <h1>{firstValue}</h1>
+      <h1>value: {secondValue}</h1> */}
+      <h1>{ text || 'john Dow' }</h1>
+      <button className="btn" onClick={()=> setIsError(!isError)}>
+        toggle error
+      </button>
+      {isError && <h1>Error...</h1>}
+      {isError ? <p>there is an error...</p> : 
+      <div>
+        <h2>There is no error.</h2>
+      </div> }
+
+      {/* {text && <h1>Hello World</h1>} 
+      {!text && <h1>Hello World</h1>} */}
+    </>
+  )
 };
 
 export default ShortCircuit;
