942a9c005cc847247b9700c95c6ca19fbb4d9343
Decong Yu <decong.yu@gmail.com>
2022-05-30 22:38:16 -0600

    prop-types

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+import defaultImage from "../../../assets/default-image.jpeg";
+
+const Product = ({ image, name, price }) => {
+	const url = image && image.url;
+	return (
+		<article className='product'>
+			<img src={url || defaultImage} alt={name || "default name"} />
+			<h4>{name || "default name"}</h4>
+			<p>${price || 9999.99}</p>
+		</article>
+	);
 };
 
+// requirement for props
+Product.propTypes = {
+	image: PropTypes.object.isRequired,
+	name: PropTypes.string.isRequired,
+	price: PropTypes.number.isRequired,
+};
+
+// // set up default props if missed
+// Product.defaultProps = {
+// 	name: "defaut name",
+// 	price: 999.99,
+// 	image: defaultImage,
+// };
 export default Product;
--- src/tutorial/9-custom-hooks/setup/2-useFetch.js
+++ src/tutorial/9-custom-hooks/setup/2-useFetch.js
@@ -14,6 +14,6 @@ export const useFetch = (url) => {
 
 	useEffect(() => {
 		getProducts();
-	}, [url]);
+	}, []);
 	return { loading, products };
 };
