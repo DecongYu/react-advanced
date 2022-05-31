33dd5e82e85314c7a2a70414fa7dc9dcb97626e6
Decong Yu <decong.yu@gmail.com>
2022-05-31 11:00:31 -0600

    Router-Navbar

--- src/tutorial/11-react-router/setup/Person.js
+++ src/tutorial/11-react-router/setup/Person.js
@@ -1,12 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import { data } from '../../../data';
-import { Link, useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { data } from "../../../data";
+import { Link, useParams } from "react-router-dom";
 const Person = () => {
-  return (
-    <div>
-      <h2>person</h2>
-    </div>
-  );
+	const [name, setName] = useState("");
+	const { id } = useParams();
+	useEffect(() => {
+		const newPerson = data.find((element) => element.id === parseInt(id));
+		setName(newPerson.name);
+	}, []);
+	return (
+		<div>
+			<h2>{name}</h2>
+			<Link to='/people' className='btn'>
+				back to People
+			</Link>
+		</div>
+	);
 };
 
 export default Person;
