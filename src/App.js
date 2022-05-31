9a863bfaec896ee386352d1306bcf30abd1c1bec
Decong Yu <decong.yu@gmail.com>
2022-05-30 20:50:20 -0600

    prop drilling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import Setup from './tutorial/6-useReducer/setup';
+import React from "react";
+import Setup from "./tutorial/7-prop-drilling/setup/1-prop-drilling";
 
 function App() {
-  return (
-    <div className='container'>
-      <Setup />
-    </div>
-  );
+	return (
+		<div className='container'>
+			<Setup />
+		</div>
+	);
 }
 
-export default App
+export default App;
